b589716027785bcdec407a071cf26029591655f0
iam.raghavan <developer.raghavan@gmail.com>
2025-04-05 21:03:38 +0530

    Add contact page with form functionality and responsive design

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 require("dotenv").config();
-
+const morgan = require("morgan");
 const pageRoutes = require("./routes/pageRoutes");
 
 const app = express();
@@ -18,6 +18,36 @@ app.set("views", path.join(__dirname, "public"));
 app.use(helmet());
 // Allow specific origin (update with your deployed domain)
 app.use(cors({ origin: "https://nve-node.onrender.com" }));
+
+app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: [
+            "'self'",
+            "maps.googleapis.com",
+            "maps.gstatic.com",
+          ],
+          imgSrc: [
+            "'self'",
+            "maps.gstatic.com",
+            "*.googleapis.com",
+            "*.ggpht.com",
+            "data:",
+          ],
+          frameSrc: ["'self'", "*.google.com"],
+          styleSrc: [
+            "'self'",
+            "'unsafe-inline'",
+            "fonts.googleapis.com",
+          ],
+          fontSrc: ["'self'", "fonts.gstatic.com"],
+        },
+      },
+    })
+  );
+
 // Optionally, allow all origins (less secure):
 // app.use(cors());
 app.use(bodyParser.json({ limit: "10kb" }));
@@ -33,6 +63,16 @@ app.use("/", pageRoutes);
 app.use((req, res) => {
     res.status(404).send("Page not found");
 });
+
+app.use(morgan("combined"));
+
+
+// Error handling middleware (catch unhandled errors)
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack to console (for debugging)
+    res.status(500).send("Something went wrong on the server.");
+});
+  
 
 // Start Server
 app.listen(PORT, () => {
